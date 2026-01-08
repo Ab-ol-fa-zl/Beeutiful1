bf15dce354536413e8f8151d8da27472ec12cafa
Ab-ol-fa-zl <113337879+Ab-ol-fa-zl@users.noreply.github.com>
2026-01-08 13:55:23 +0100

    Scene update

---
+++ Assets/Scripts/NPC_patrol.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using UnityEngine;
+
+public class NPC-patrol : MonoBehaviour
+{
+    public vector2{} patrolPoints;
+    
+
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
